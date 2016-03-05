31618ede2fb8545dfb633e4079bf60a61985b3f3
ズッカーマンまや <zukkamanakiko@zukkamanakiko-no-MacBook-Pro.local>
2016-03-05 21:42:06 +0900

    languagesのホバーともっと詳しくの表示

--- script.js
+++ script.js
@@ -1,3 +1,55 @@
 $(function() {
 
+// ここからtopのもっと詳しく
+  $('#read-more').click(function() {
+  	if ($(this).hasClass('open')) {
+  		$('#more-text').fadeOut();
+  		$('#read-more').removeClass('open').text('もっと詳しく');
+  	} else {
+  		$("#more-text").fadeIn();
+        $("#read-more").addClass("open").text("閉じる");
+  	}
+  });
+// ここまでtopのもっと詳しく
+
+
+
+
+//  ここからlanguagesのホバー
+   $('.languages-hover').hover(
+    function() {
+    	console.log($(this).find('.languages-detail'))
+      
+      $(this).find('.languages-detail').fadeIn(500);
+      
+    },
+    function() {
+     
+      $(this).find('.languages-detail').fadeOut(500);
+      
+      
+    }
+  );
+//  ここまでlanguagesのホバー
+
+
+
+
+
+
+
 });
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
