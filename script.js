331f0941c33daef170dbd579fbac78017d0b2dd6
ズッカーマンまや <zukkamanakiko@zukkamanakiko-no-MacBook-Pro.local>
2016-03-07 22:26:23 +0900

    スクロールナビできた！

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 $(function() {
 
+//ページ内スクロール
+    $("header a").click(function () {
+        var i = $(".header-menu a").index(this)
+        var p = $(".scroll").eq(i).offset().top; 
+        $('html,body').animate({ scrollTop: p }, 500);
+        return false;
+    });
+// fin
+
+
+ 
+
 // ここからtopのもっと詳しく
   $('#read-more').click(function() {
   	if ($(this).hasClass('open')) {
@@ -10,7 +22,7 @@ $(function() {
         $("#read-more").addClass("open").text("閉じる");
   	}
   });
-// ここまでtopのもっと詳しく
+// fin
 
 
 
@@ -30,12 +42,30 @@ $(function() {
       
     }
   );
-//  ここまでlanguagesのホバー
+//  fin
+
+//  ここからコラムの画像のホバー
+   $('.img-zoom').hover(
+    function() {
+    	console.log($(this).addClass('zoom'))
+
+      $(this).addClass('zoom');
+      $(this).children('.zoom-black').fadeIn(300);
+      
+    },
+    function() {
+      $(this).removeClass('zoom');
+      $(this).children('.zoom-black').fadeOut(300);
+      
+      
+    }
+  );
 
 
 
 
 
+//  fin
 
 
 });
